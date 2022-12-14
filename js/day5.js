5975280ccfd68041039439a48058fdfad4bb1bc7
Rijan Tiwari <reezantiwaree@gmail.com>
2022-12-14 15:30:16 +0545

    array of objects

---
+++ js/day5.js
@@ -0,0 +1,17 @@
+let arr1 =[
+        {
+            "fname":"rijan",
+            "lname":"tiwari"
+
+        }, {
+            "fname":"Hello",
+            "lname":"world"
+        },
+        {
+            "fname":"Hi",
+            "lname":"Hello"
+        }
+]
+
+console.log(arr1[0])
+console.log(arr1[0].fname)
