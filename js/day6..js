5fa825430660521f5c6c0ddcd490f4223a7f974f
Rijan Tiwari <reezantiwaree@gmail.com>
2022-12-15 16:48:32 +0545

    Array of method

--- js/day6..js
+++ js/day6..js
@@ -23,7 +23,8 @@
 //        }
 // }
 // myFunc();
-let arr=[1,2,3,4,5,6,7,8,9,10]
-for(let i=0; i < arr.length;i++){
-    console.log(arr[i], "index " + i);
-}
+// let arr=[1,2,3,4,5,6,7,8,9,10]
+// for(let i=0; i < arr.length;i++){
+//     console.log(arr[i], "index " + i);
+// }
+
---
+++ js/day7.js
@@ -0,0 +1,41 @@
+// let operator = prompt("Enter operator (+, -, /, * )");
+// let firstNum = parseInt (prompt("Enter First Number: "));
+// let secondNum = parseInt(prompt("Enter Second Number: "));
+// let result;
+// switch(operator){
+//             case "-":
+//                 result = firstNum - secondNum;
+//                 console.log(`${firstNum} - ${secondNum} = ${result}` );
+//                 break;
+//             case "+":
+//                     result = firstNum + secondNum;
+//                     console.log(`${firstNum} + ${secondNum} = ${result}` );
+//                     break;
+//             case "*":
+//                         result = firstNum * secondNum;
+//                         console.log(`${firstNum} * ${secondNum} = ${result}` );
+//                         break;
+//             case "/":
+//                          result = firstNum / secondNum;
+//                          console.log(`${firstNum} + ${secondNum} = ${result}` );
+//                             break;
+
+//             default:
+//                 console.log("Please choose correct operator");
+//                 break;
+
+// }
+
+// methods of array 
+
+// let arr=[1,2,3];
+// arr.push(5);
+// arr.unshift(0);
+// for(let i=0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+// arr.shift();
+// arr.pop();
+// for(let i=0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
