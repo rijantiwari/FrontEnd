c657cc313e147d0d1e155052891c0bcf9218a736
Rijan Tiwari <reezantiwaree@gmail.com>
2022-12-15 16:13:46 +0545

    loop

---
+++ js/day6..js
@@ -0,0 +1,29 @@
+// let personName={
+//         myname:"rijan",
+//         age:20,
+       
+// }
+
+// if(personName.age > 20){
+//   alert(personName.myname);
+// }
+// else if( personName.age < 20){
+//     alert(personName.myname);
+
+// }
+// else{
+//     alert("failed to execute");
+// }
+
+// function myFunc (){
+//         let a = 10;
+//         let b = 9;
+//        if(a >b){
+//          alert("A is greater");
+//        }
+// }
+// myFunc();
+let arr=[1,2,3,4,5,6,7,8,9,10]
+for(let i=0; i < arr.length;i++){
+    console.log(arr[i], "index " + i);
+}
