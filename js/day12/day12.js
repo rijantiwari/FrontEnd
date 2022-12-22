5b592e94ee70834307e608cddf992ed37929520f
Rijan Tiwari <reezantiwaree@gmail.com>
2022-12-22 16:56:55 +0545

    day 12 js

--- js/day12/day12.js
+++ js/day12/day12.js
@@ -6,3 +6,51 @@ button.addEventListener("click",(event)=> {
 }
 
 )
+
+// // default params
+// function add(num1, num2=10){
+//     return num1 + num2;
+// }
+// console.log(add (5));
+
+// let myName="John";
+// let myRole="student"
+
+// destructuring
+
+// let array = ["Rijan", "Student"];
+// const[myName, myRole]=array;
+// console.log(myName,"myName");
+// // object desstructuring 
+// const object2={name:"rijan",caste:"Tiwari"}
+// const{caste} = object2;
+// console.log(caste,"caste")
+
+// spread operator
+
+// let arr = [1,2,3,4,5,6,7,7];
+// let newarr = [...arr];
+// console.log(newarr)
+
+// console.log(sum(arr));
+// function sum(...add){
+//     let result = 0;
+//     for(let i=0;i < add.length; i++){
+//         sum+=add[i];
+//     }
+//     return result;
+// }
+
+// const myVehicle={
+//     brand:"Ford",
+//     model:"Mustang",
+//     color:"red"
+// }
+// const updateMyVehicle={
+//     type:"car",
+//     year:2021,
+//     color:"Yellow",
+// }
+// const copyVariable = {...myVehicle,...updateMyVehicle};
+// console.log(copyVariable)
+// deep copy and shallow copy
