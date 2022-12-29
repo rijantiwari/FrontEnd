7222d0e76fd2e14d2620b2fbc6edef14819cf30a
Rijan Tiwari <reezantiwaree@gmail.com>
2022-12-29 18:25:15 +0545

    Registration Form

---
+++ React/form/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import RegistrationForm from './RegistrationForm';
+
+function App() {
+  return (
+    <div className="App">
+     <RegistrationForm/>
+   
+    </div>
+  );
+}
+
+export default App;
