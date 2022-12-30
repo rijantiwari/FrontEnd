ff4fa5959ec6762261f02375f9bab4fc244ee67e
Rijan Tiwari <reezantiwaree@gmail.com>
2022-12-30 15:14:02 +0545

    New counter app

---
+++ React/shoppingcounter/src/components/Counter.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Counter extends Component {
+  state ={
+    count:0
+  };
+  formatCount(){
+    const{count}=this.state;
+   return count === 0 ? "Zero":count;
+  }
+  render() {
+    return (
+      <> 
+        <span className='badge badge-primary m-2'> {this.formatCount()} </span>
+        <button className='btn btn-secondary btn-sm'> Increment</button>
+      </>
+     
+    )
+  }
+}
