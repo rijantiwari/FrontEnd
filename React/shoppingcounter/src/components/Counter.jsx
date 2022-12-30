d506e0e3949f8f93322c2019d770e38ce11a7f4e
Rijan Tiwari <reezantiwaree@gmail.com>
2022-12-30 15:22:19 +0545

    new changes

--- React/shoppingcounter/src/components/Counter.jsx
+++ React/shoppingcounter/src/components/Counter.jsx
@@ -4,17 +4,25 @@ export default class Counter extends Component {
   state ={
     count:0
   };
+
   formatCount(){
     const{count}=this.state;
    return count === 0 ? "Zero":count;
   }
   render() {
+
     return (
       <> 
-        <span className='badge badge-primary m-2'> {this.formatCount()} </span>
+        <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
         <button className='btn btn-secondary btn-sm'> Increment</button>
       </>
      
     )
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 }
