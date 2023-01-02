7f0a4fc70476a026e4f32fb48f23917f0c3cc698
Rijan Tiwari <reezantiwaree@gmail.com>
2023-01-02 16:39:33 +0545

    useEffect

--- React/form/src/App.js
+++ React/form/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import RegistrationForm from './RegistrationForm';
+import EffectHook from './EffectHook';
 
 function App() {
   return (
     <div className="App">
-     <RegistrationForm/>
-   
+     {/* <RegistrationForm/> */}
+     <EffectHook />
     </div>
   );
 }
---
+++ React/form/src/EffectHook.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useState,useEffect } from 'react';
+
+function EffectHook() {
+    const [count, setCount] = useState(0);
+    const increaseCount = () => {
+      setCount((prevCount) => prevCount + 1)
+    }
+    // const decreaseCount = () => {
+    //   setCount((prevCount) => prevCount - 1)
+    // }
+    // const restartCount = () => {
+    //   setCount(0)
+    // }
+    useEffect(() => {
+        document.title =`I am running ${count} times`
+    //   const interval = setInterval(() => {
+    //     setCount(count + 1);
+    //   }, 1000);
+    console.log("I am running");
+
+    },[]);
+  
+    return (
+      <>
+        <div>
+          <h1>CounterApp</h1>
+          <p>I have rendered {count} times.</p>
+          <button className="increaseCount" onClick={increaseCount}>+</button>
+          {/* <button className="decreaseCount" onClick={decreaseCount}>-</button><br/><br/>
+          <button className="resetCount" onClick={restartCount}>Restart</button> */}
+        </div>
+      </>
+    )
+}
+
+export default EffectHook
