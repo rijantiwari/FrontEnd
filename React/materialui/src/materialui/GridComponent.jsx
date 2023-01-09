8d0fa5a9e9d06575271ae440c16d32c0a8433b32
Rijan Tiwari <reezantiwaree@gmail.com>
2023-01-09 17:23:20 +0545

    appbar

--- React/materialui/src/materialui/GridComponent.jsx
+++ React/materialui/src/materialui/GridComponent.jsx
@@ -1,10 +1,15 @@
-import { CssBaseline } from '@mui/material'
+import { AppBar, CssBaseline, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 
 const GridComponent = () => {
   return (
     <>
     <CssBaseline />
+    <AppBar position="relative">
+        <Toolbar>
+            <Typography variant='h6'> Posts </Typography>
+        </Toolbar>
+     </AppBar>
     
     </>
   )
