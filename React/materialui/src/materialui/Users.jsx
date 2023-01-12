261113002a422ee77572d1b37f5ef23736004dfe
Rijan Tiwari <reezantiwaree@gmail.com>
2023-01-12 16:49:02 +0545

    pagination updated

--- React/materialui/src/materialui/Users.jsx
+++ React/materialui/src/materialui/Users.jsx
@@ -8,11 +8,13 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { useState } from "react";
 import { useEffect } from "react";
-import { CircularProgress } from "@mui/material";
+import { CircularProgress, Pagination } from "@mui/material";
 
 export default function Users() {
   const [users, setUsers] = useState([]);
   const [loadingusers, setLoadingUsers] = useState([]);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [page, setPage] = useState(1);
   const getUserData = () => {
     setLoadingUsers(true);
     fetch("https://63b06aa0f9a53fa20268c6ed.mockapi.io/api/v1/users")
@@ -27,37 +29,51 @@ export default function Users() {
     getUserData();
   }, []);
   console.log(users);
+  const handleChangePage = (event, newPage) => {
+    console.log(newPage, "new page");
+    setPage(newPage);
+  };
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Sn.</TableCell>
-            <TableCell>FirstName</TableCell>
-            <TableCell>LastName</TableCell>
-          </TableRow>
-        </TableHead>
-        {loadingusers ? (
-          <CircularProgress />
-        ) : (
-          <TableBody>
-            {users?.map((user) => {
-              return (
-                <TableRow
-                  key={user.id}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {user.id}
-                  </TableCell>
-                  <TableCell>{user.firstname}</TableCell>
-                  <TableCell>{user.lastname}</TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        )}
-      </Table>
-    </TableContainer>
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Sn.</TableCell>
+              <TableCell>FirstName</TableCell>
+              <TableCell>LastName</TableCell>
+            </TableRow>
+          </TableHead>
+          {loadingusers ? (
+            <CircularProgress />
+          ) : (
+            <TableBody>
+              {users
+                .slice(page * rowsPerPage - rowsPerPage, rowsPerPage * page)
+                .map((user) => {
+                  return (
+                    <TableRow
+                      key={user.id}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {user.id}
+                      </TableCell>
+                      <TableCell>{user.firstname}</TableCell>
+                      <TableCell>{user.lastname}</TableCell>
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          )}
+        </Table>
+      </TableContainer>
+
+      <Pagination
+        count={Math.ceil(users?.length / rowsPerPage)}
+        onChange={handleChangePage}
+        color="primary"
+      />
+    </>
   );
 }
