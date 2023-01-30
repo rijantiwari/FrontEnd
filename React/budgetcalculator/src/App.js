fccdfb78600be52a5d3bfb88fd651e7dfeca0291
rijantiwari <reezantiwaree@gmail.com>
2023-01-30 13:31:49 +0545

    ui

--- React/budgetcalculator/src/App.js
+++ React/budgetcalculator/src/App.js
@@ -1,9 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseList from './components/ExpenseList';
+import Alert from './components/Alert';
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
+const initialExpenses=[
+    {id:uuidv4(),charge:"rent",amount:1500},
+    {id:uuidv4(),charge:"Car Payment",amount:500},
+    {id:uuidv4(),charge:"Credit Card Bill",amount:1200},
+];
+
 
 function App() {
-    return (
+    const[expenses, setExpenses]=useState(initialExpenses);
 
+    return (
+        <>
+        <Alert/>
+        <h1> budget calculator</h1>
+        <main className='App'>
+            <ExpenseForm/>
+           <ExpenseList expenses={expenses}/>
+        </main>
+        <h1>
+            total spending: <span className='total'>
+                $ {expenses.reduce((acc,curr)=>{
+                    return (acc += curr.amount);
+                },0)}
+            </span>
+        </h1>
+        </>
+        
     );
 }
 
---
+++ React/budgetcalculator/src/components/Alert.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Alert = () => {
+  return (
+    <div> Hello From Alert</div>
+  )
+}
+
+export default Alert
