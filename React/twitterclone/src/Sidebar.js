7b7497e21f24146973c59c49d887b7b113d518a8
rijantiwari <reezantiwaree@gmail.com>
2023-06-02 13:13:09 +0545

    clone

---
+++ React/twitterclone/src/Sidebar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Sidebar.css";
+import TwitterIcon from "@mui/icons-material/Twitter";
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* Twitter Icon*/}
+      <TwitterIcon />
+      {/*SidebaOption */}
+      {/*SidebaOption */}
+      {/*SidebaOption */}
+      {/*SidebaOption */}
+      {/*SidebaOption */}
+      {/*SidebaOption */}
+      {/*Tweet Button*/}
+    </div>
+  );
+}
+
+export default Sidebar;
