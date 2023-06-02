5e4e18b9cdf93fe40d0b434849046d8166319db9
rijantiwari <reezantiwaree@gmail.com>
2023-06-02 15:22:43 +0545

    clone

--- React/twitterclone/src/App.js
+++ React/twitterclone/src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
+import Feed from "./Feed";
 import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
-      {/* Sidebar*/}
       <Sidebar />
-
-      {/* Feed*/}
-
-      {/* Widget */}
+      <Feed />
     </div>
   );
 }
---
+++ React/twitterclone/src/Feed.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Feed() {
+  return (
+    <div>
+      {/* Header */}
+      <h2> Home</h2>
+      {/* TweetB */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+    </div>
+  );
+}
+
+export default Feed;
